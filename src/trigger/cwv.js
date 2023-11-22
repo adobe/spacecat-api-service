bfdc7b69dae8e2d44bf941570cacf2051e7ea64f
Ekrem Doğan <ekremney@users.noreply.github.com>
2023-11-22 22:51:55 +0100

    Audit context (#13)

--- src/trigger/cwv.js
+++ src/trigger/cwv.js
@@ -12,6 +12,7 @@
 
 import { createUrl, Response } from '@adobe/fetch';
 import { fetch } from '../support/utils.js';
+import { postSlackMessage } from '../support/slack.js';
 
 export const DEFAULT_PARAMS = { // export for testing
   interval: 30,
@@ -20,30 +21,63 @@ export const DEFAULT_PARAMS = { // export for testing
 };
 
 const DOMAIN_LIST_URL = 'https://helix-pages.anywhere.run/helix-services/run-query@v3/dash/domain-list';
-const ALL_URLS = 'ALL';
+export const INITIAL_SLACK_MESSAGE = '*PERFORMANCE DEGRADATION (CWV) REPORT* for the *last week* :thread:';
+// fallback slack channel (franklin-spacecat-internal-test) hardcoded to use when no appropriate
+// slack channel was provided as parameter
+export const FALLBACK_SLACK_CHANNEL = 'C060T2PPF8V';
 
-export default async function triggerCWVAudit(context) {
-  const { log, sqs } = context;
-  const { type, url } = context.data;
-  const {
-    RUM_DOMAIN_KEY: domainkey,
-    AUDIT_JOBS_QUEUE_URL: queueUrl,
-  } = context.env;
+function isAuditForAll(url) {
+  return url.toUpperCase() === 'ALL';
+}
 
-  if (!domainkey || !queueUrl) {
-    throw Error('Required env variables is missing');
-  }
+/**
+ * Destructs the env variable in name1=lid1,name2=id2 comma separated pairs and matches the
+ * channel name which is provided by the target variable. Then returns the matched channel id
+ * If no channel is matched to the given target param, then id of the fallback channel is returned
+ * @param target name of the channel to match
+ * @param targetChannels env variable in name=id,name=id form
+ * @returns {*|string}
+ */
+export function getSlackChannelId(target, targetChannels = '') {
+  const channel = targetChannels.split(',')
+    .filter((pair) => pair.startsWith(`${target}=`))
+    .find((pair) => pair.trim().length > target.length + 1);
+  return channel ? channel.split('=')[1].trim() : FALLBACK_SLACK_CHANNEL;
+}
 
+async function fetchDomainList(domainkey, url) {
   const params = {
     ...DEFAULT_PARAMS,
     domainkey,
   };
 
   const resp = await fetch(createUrl(DOMAIN_LIST_URL, params));
-  const respJson = await resp.json();
+  const respJson = await resp.json(); // intentionally not catching parse error here.
 
-  const filteredUrls = respJson?.results?.data?.map((result) => result.hostname)
-    .filter((respUrl) => url.toUpperCase() === ALL_URLS || url === respUrl);
+  const data = respJson?.results?.data;
+  if (!Array.isArray(data)) {
+    throw new Error('Unexpected response format. $.results.data is not array');
+  }
+
+  const urls = data.map((row) => row.hostname);
+  return isAuditForAll(url) ? urls : urls.filter((row) => url === row);
+}
+
+export default async function triggerCWVAudit(context) {
+  const { log, sqs } = context;
+  const { type, url, target } = context.data;
+  const {
+    RUM_DOMAIN_KEY: domainkey,
+    AUDIT_JOBS_QUEUE_URL: queueUrl,
+    TARGET_SLACK_CHANNELS: targetChannels,
+    SLACK_BOT_TOKEN: token,
+  } = context.env;
+
+  if (!domainkey || !queueUrl) {
+    throw Error('Required env variables is missing');
+  }
+
+  const filteredUrls = await fetchDomainList(domainkey, url);
 
   if (filteredUrls.length === 0) {
     return new Response('', {
@@ -54,9 +88,23 @@ export default async function triggerCWVAudit(context) {
     });
   }
 
+  let slackContext;
+  // if audit triggered for all urls, then an initial message sent to the channel and the slack
+  // thread id is added to auditContext for downstream components to send messages under same thread
+  // If audit is triggered for a single url, then only channel id is added to uudit context
+  const channelId = getSlackChannelId(target, targetChannels);
+  if (isAuditForAll(url)) {
+    slackContext = await postSlackMessage(channelId, INITIAL_SLACK_MESSAGE, token);
+  } else {
+    slackContext = { channel: channelId };
+  }
+
   for (const filteredUrl of filteredUrls) {
+    const auditContext = {
+      ...(slackContext && { slackContext }),
+    };
     // eslint-disable-next-line no-await-in-loop
-    await sqs.sendMessage(queueUrl, { type, url: filteredUrl });
+    await sqs.sendMessage(queueUrl, { type, url: filteredUrl, auditContext });
   }
 
   const message = `Successfully queued ${type} audit jobs for ${filteredUrls.length} url/s`;
