4f6e6207b2aabacb717e8bdf111971a6fb85e94c
Dominique Jäggi <1872195+solaris007@users.noreply.github.com>
2023-12-22 17:34:09 +0100

    fix: allow headers (#69)

--- test/index.test.js
+++ test/index.test.js
@@ -72,7 +72,7 @@ describe('Index Tests', () => {
     expect(resp.status).to.equal(204);
     expect(resp.headers.plain()).to.eql({
       'access-control-allow-methods': 'GET, HEAD, PATCH, POST, OPTIONS, DELETE',
-      'access-control-allow-headers': 'x-api-key',
+      'access-control-allow-headers': 'x-api-key, origin, x-requested-with, content-type, accept',
       'access-control-max-age': '86400',
       'access-control-allow-origin': '*',
       'content-type': 'application/json; charset=utf-8',
