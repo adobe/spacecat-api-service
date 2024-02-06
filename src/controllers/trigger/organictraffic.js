9feafcf1f2e8beae64c7b69bf39c5cd96314ea9d
Iulia Grumaz <iuliag@users.noreply.github.com>
2024-02-06 12:56:27 +0200

    feat: add organic traffic audit type (SITES-18661) (#150)
    Add organic traffic audit type
    
    ## Related Issues
    https://jira.corp.adobe.com/browse/SITES-18661

---
+++ src/controllers/trigger/organictraffic.js
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2024 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+import { DELIVERY_TYPES } from '@adobe/spacecat-shared-data-access/src/models/site.js';
+import { triggerFromData } from './common/trigger.js';
+import { getSlackContext } from '../../utils/slack/base.js';
+
+export const INITIAL_ORGANIC_TRAFFIC_SLACK_MESSAGE = '*ORGANIC TRAFFIC REPORT* for the *last week* :thread:';
+
+/**
+ * Triggers audit processes for websites based on the provided URL.
+ *
+ * @param {Object} context - The context object containing dataAccess, sqs, data, and env.
+ * @returns {Response} The response object with the audit initiation message or an error message.
+ */
+export default async function trigger(context) {
+  const { log } = context;
+
+  const { type, url } = context.data;
+  const {
+    AUDIT_REPORT_SLACK_CHANNEL_ID: slackChannelId,
+    SLACK_BOT_TOKEN: token,
+  } = context.env;
+
+  const slackContext = await getSlackContext({
+    slackChannelId, url, message: INITIAL_ORGANIC_TRAFFIC_SLACK_MESSAGE, token, log,
+  });
+
+  const auditContext = {
+    slackContext,
+  };
+
+  const config = {
+    url,
+    auditTypes: [type],
+    deliveryType: DELIVERY_TYPES.AEM_EDGE,
+  };
+
+  return triggerFromData(context, config, auditContext);
+}
