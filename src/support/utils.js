d7c22d3f17e5e20eab38025423a372366dd2c558
Dominique Jäggi <djaeggi@adobe.com>
2024-05-28 10:49:15 +0200

    fix: set desktop mode

--- src/support/utils.js
+++ src/support/utils.js
@@ -57,7 +57,7 @@ export const sendExperimentationCandidatesMessage = async (
   url,
   slackContext,
 ) => sqs.sendMessage(queueUrl, {
-  processingType: 'experimentation-candidates',
+  processingType: 'experimentation-candidates-desktop',
   urls: [{ url }],
   slackContext,
 });
