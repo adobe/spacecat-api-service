1ad5428ba9c8314724a61f7a63e8b30836b99d6e
Sweta Barman <swbarman@adobe.com>
2024-06-19 09:38:21 -0400

    feat: accept import.js as Base64 encoded string to /tools/import endpoint

--- src/controllers/import.js
+++ src/controllers/import.js
@@ -105,8 +105,8 @@ function ImportController(context) {
       validateImportApiKey(importApiKey);
       validateRequestData(data);
 
-      const { urls, options } = data;
-      const job = await importSupervisor.startNewJob(urls, importApiKey, options);
+      const { urls, options, importScript } = data;
+      const job = await importSupervisor.startNewJob(urls, importApiKey, options, importScript);
 
       return createResponse(ImportJobDto.toJSON(job), STATUS_ACCEPTED);
     } catch (error) {
