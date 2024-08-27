b4a27c4a8e4dd6c9d9bbed42f19eceac03880873
Sahil Silare <sahilsilare@gmail.com>
2024-08-27 11:28:24 +0530

    chore: added export from scraper

--- src/support/slack/commands.js
+++ src/support/slack/commands.js
@@ -18,6 +18,7 @@ import getSites from './commands/get-sites.js';
 import martechImpact from './commands/martech-impact.js';
 import runAudit from './commands/run-audit.js';
 import runImport from './commands/run-import.js';
+import runScrape from './commands/run-scrape.js';
 import setLiveStatus from './commands/set-live-status.js';
 import help from './commands/help.js';
 
@@ -36,6 +37,7 @@ export default (context) => [
   martechImpact(context),
   runAudit(context),
   runImport(context),
+  runScrape(context),
   setLiveStatus(context),
   help(context),
 ];
