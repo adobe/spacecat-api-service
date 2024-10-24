47a92fa9c1c9e56fcab791b9616c123bd4b7241f
Valerii Naida <vnayda@adobe.com>
2024-10-24 18:11:33 -0500

    `feat: SITES-25573 Enable/Disable domains for CWV audit via API/slack command
    -- debug`

--- src/support/slack/commands/enable-audit.js
+++ src/support/slack/commands/enable-audit.js
@@ -9,21 +9,9 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-// import { createConfiguration }
-// from '@adobe/spacecat-shared-data-access/src/models/configuration.js';
-// import { ConfigurationDto as DynamoConfigurationDto }
-// from '@adobe/spacecat-shared-data-access/src/dto/configuration.js';
 import BaseCommand from './base.js';
-import { extractURLFromSlackInput } from '../../../utils/slack/base.js';
-import { ConfigurationDto } from '../../../dto/configuration.js';
 
-const PHRASES = ['enable-audit'];
-
-// function incrementVersion(version) {
-//   if (!version) return 'v1';
-//   const versionNumber = parseInt(version.substring(1), 10);
-//   return `v${versionNumber + 1}`;
-// }
+const PHRASES = ['check-configuration-version'];
 
 export default (context) => {
   const baseCommand = BaseCommand({
@@ -31,48 +19,18 @@ export default (context) => {
     name: 'Audit enabling test command',
     description: '',
     phrases: PHRASES,
-    usageText: `${PHRASES[0]} {site} {auditType1}`,
+    usageText: `${PHRASES[0]}`,
   });
 
   const { dataAccess } = context;
 
   const handleExecution = async (args, slackContext) => {
     const { say } = slackContext;
-    const [baseURLInput, auditType] = args;
-    const baseURL = extractURLFromSlackInput(baseURLInput);
 
-    const configuration = await dataAccess.getConfiguration();
-    const site = await dataAccess.getSiteByBaseURL(extractURLFromSlackInput(baseURL));
+    const latestConfiguration = await dataAccess.getConfiguration();
+    const specificVersionConfiguration = await dataAccess.getConfigurationByVersion('v10');
 
-    // Before update
-    let isAuditEnabled = configuration.isHandlerEnabledForSite(auditType, site);
-    await say(`[Before update]: Is audit enabled: ${auditType} ${isAuditEnabled}. Version: ${configuration.getVersion()}`);
-
-    // Update
-    configuration.enableHandlerForSite(auditType, site);
-
-    const configurationData = ConfigurationDto.toJSON(configuration);
-    await dataAccess.updateConfiguration(configurationData);
-
-    // Emulate logic from dataAccess
-    // const newConfigurationData = { ...configurationData };
-    // const latestConfiguration = await dataAccess.getConfiguration();
-    // newConfigurationData.version = incrementVersion(latestConfiguration?.getVersion());
-    // const newConfiguration = createConfiguration(newConfigurationData);
-    // const configurationDynamoItem = DynamoConfigurationDto.toDynamoItem(newConfiguration);
-    // await say(`Dynamo configuration item: \n\n ${JSON.stringify(configurationDynamoItem)}`);
-
-    // After update
-    isAuditEnabled = configuration.isHandlerEnabledForSite(auditType, site);
-    await say(`[After update]: Is audit enabled: ${auditType} ${isAuditEnabled}. Version: ${configuration.getVersion()}`);
-
-    // Reload
-    const reloadedConfiguration = await dataAccess.getConfigurationByVersion('v10');
-    const handler = reloadedConfiguration.getHandlers()['broken-backlinks-external'];
-    await say(`Reloaded configuration "broken-backlinks-external" handler: \n\n ${JSON.stringify(handler)}`);
-
-    isAuditEnabled = reloadedConfiguration.isHandlerEnabledForSite(auditType, site);
-    await say(`[After reload]: Is audit enabled: ${auditType} ${isAuditEnabled}. Version: ${reloadedConfiguration.getVersion()}`);
+    await say(`Latest Configuration Version: ${latestConfiguration.getVersion}, \n\n but we have : ${specificVersionConfiguration.getVersion()}`);
   };
 
   baseCommand.init(context);
