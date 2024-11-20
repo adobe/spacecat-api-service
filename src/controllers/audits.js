9160abfe3c483bedd13390d78b6a8fac20237226
Valerii Naida <vnayda@adobe.com>
2024-11-19 22:34:03 -0600

    feat: Introduce configurable grouping patterns

--- src/controllers/audits.js
+++ src/controllers/audits.js
@@ -147,6 +147,8 @@ function AuditsController(dataAccess) {
       return badRequest('Audit type required');
     }
 
+    console.log(context);
+    console.log(context.data);
     const { excludedURLs, manualOverwrites, groupedURLs } = context.data;
     let hasUpdates = false;
 
