5a128ffe5972f94f62a387c95ebb5ed9d018e726
Francisco Chicharro Sanz <chicharr@adobe.com>
2024-11-25 15:11:52 +0100

    chore(fix): check for null in oppty.getSuggestions()

--- src/controllers/opportunities.js
+++ src/controllers/opportunities.js
@@ -225,7 +225,8 @@ function OpportunitiesController(dataAccess) {
 
     // TODO: eventually suggestions will be removed by the data access layer
     // and this code should be removed
-    const removeSuggestionsPromises = await (opportunity.getSuggestions())
+    const suggestionsArray = await opportunity.getSuggestions();
+    const removeSuggestionsPromises = suggestionsArray
       .map(async (suggestion) => suggestion.remove());
 
     await Promise.all(removeSuggestionsPromises);
