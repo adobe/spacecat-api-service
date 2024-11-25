2a8305872b0f17f638744577413f228415b61ae6
Francisco Chicharro Sanz <chicharr@adobe.com>
2024-11-25 14:54:06 +0100

    chore(fix): opportunity getSuggestions

--- src/controllers/opportunities.js
+++ src/controllers/opportunities.js
@@ -225,7 +225,7 @@ function OpportunitiesController(dataAccess) {
 
     // TODO: eventually suggestions will be removed by the data access layer
     // and this code should be removed
-    const removeSuggestionsPromises = await opportunity.getSuggestions()
+    const removeSuggestionsPromises = await (opportunity.getSuggestions())
       .map(async (suggestion) => suggestion.remove());
 
     await Promise.all(removeSuggestionsPromises);
