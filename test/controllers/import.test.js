b3161c8456a6a9fa8867e934f0b7ac41e746ce19
Sweta Barman <swbarman@adobe.com>
2024-11-26 14:49:13 -0500

    fix: Fix failing unit test

--- test/controllers/import.test.js
+++ test/controllers/import.test.js
@@ -728,7 +728,7 @@ describe('ImportController tests', () => {
 
       expect(response).to.be.an.instanceOf(Response);
       expect(response.status).to.equal(400);
-      expect(response.headers.get('x-error')).to.equal('Invalid request: Patch request value needs to be "STOPPED"');
+      expect(response.headers.get('x-error')).to.equal('Invalid request: Patch request supports the following values: ["STOPPED"]');
     });
     it('should fail when import job is not provided', async () => {
       baseContext.dataAccess.getImportJobByID = sandbox.stub().resolves(null);
