edc8079c7648dcfdbb8083a6cb8d138e88a8f8a0
Sahil Silare <ssilare@adobe.com>
2025-01-08 14:35:01 +0530

    fix: changed message
    Signed-off-by: Sahil Silare <ssilare@adobe.com>

--- src/support/slack/commands/run-scrape.js
+++ src/support/slack/commands/run-scrape.js
@@ -96,7 +96,7 @@ function RunScrapeCommand(context) {
         slackContext,
         context,
       ));
-      await Promise.all(promises).then(() => say(`:adobe-run: Triggered scrape run for site \`${baseURL}\``));
+      await Promise.all(promises).then(() => say(`:adobe-run: Triggering scrape run for site \`${baseURL}\``));
       await say(`:white_check_mark: Completed triggering scrape runs for site \`${baseURL}\` — Total URLs: ${urls.length}`);
     } catch (error) {
       log.error(error);
