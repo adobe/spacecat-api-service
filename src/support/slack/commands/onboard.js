203aba8fd4461dc69eeb077ca950d7ae2aa68e3d
Aliaksandra Kupreyeva <akupreyeva@adobe.com>
2025-02-25 15:12:59 +0100

    add a say call for debugging purposes

--- src/support/slack/commands/onboard.js
+++ src/support/slack/commands/onboard.js
@@ -219,6 +219,7 @@ function OnboardCommand(context) {
           /* eslint-disable no-await-in-loop */
           const [baseURL, imsOrgID] = row;
           const reportLine = await onboardSingleSite(baseURL, imsOrgID, profileName, slackContext);
+          await say(`Onboarding a site with base URL ${baseURL} and IMS org ID ${imsOrgID}`);
           fileStream.write(csvStringifier.stringifyRecords([reportLine]));
         }
 
