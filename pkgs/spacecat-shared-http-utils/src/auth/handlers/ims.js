669939fc6bbe96951a3ae8aed4ecbdea1191feef
David Bosschaert <bosschae@adobe.com>
2025-03-18 14:51:05 +0000

    wip

--- pkgs/spacecat-shared-http-utils/src/auth/handlers/ims.js
+++ pkgs/spacecat-shared-http-utils/src/auth/handlers/ims.js
@@ -260,6 +260,11 @@ export default class AdobeImsHandler extends AbstractHandler {
     const { log } = context;
     return createDataAccess({
       tableNameData: 'spacecat-services-rbac-dev',
+      aclCtx: {
+        aclEntities: {
+          exclude: ['acl', 'role'],
+        },
+      },
     }, log);
   }
 
