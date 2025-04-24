85dfc7b4734e914730d269ff161b6fb2440b7d07
David Aurelio <158859+davidaurelio@users.noreply.github.com>
2025-04-24 17:27:09 +0200

    fix: Support SQS Queue Names (#877)
    We run with a mixture of SQS queue names and urls in all environment.
    
    Since we have been passing queue URLs through to the AWS SDK as is, raw queue names lead to errors rather than successful queue operations.
    
    The changes introduced here first check whether the provided queue url or name can be parsed as URL, and if not, tries to retrieve the url for the named queue.
    
    ## Related Issues
    https://jira.corp.adobe.com/browse/SITES-30735

--- src/support/sqs.js
+++ src/support/sqs.js
@@ -9,25 +9,38 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-import { SendMessageCommand, SQSClient, PurgeQueueCommand } from '@aws-sdk/client-sqs';
+import {
+  SendMessageCommand, SQSClient, PurgeQueueCommand, GetQueueUrlCommand,
+} from '@aws-sdk/client-sqs';
 
 /**
  * @class SQS utility to send messages to SQS
  * @param {string} region - AWS region
  * @param {object} log - log object
  */
-class SQS {
+export class SQS {
+  /** @type {Map<string, Promise<string>>} */
+  #namedQueueURLs = new Map();
+
   constructor(region, log) {
     this.sqsClient = new SQSClient({ region });
     this.log = log;
   }
 
-  async sendMessage(queueUrl, message) {
+  /**
+   * Send a message to the specified SQS queue.
+   *
+   * @param {string|URL} queueNameOrUrl - The name or URL of the SQS queue.
+   * @param {object} message - The message to send.
+   * @returns {Promise<void>} - Promise that resolves when the message is sent.
+   */
+  async sendMessage(queueNameOrUrl, message) {
     const body = {
       ...message,
       timestamp: new Date().toISOString(),
     };
 
+    const queueUrl = await this.#toQueueUrl(queueNameOrUrl);
     const msgCommand = new SendMessageCommand({
       MessageBody: JSON.stringify(body),
       QueueUrl: queueUrl,
@@ -45,11 +58,13 @@ class SQS {
 
   /**
    * Purge the queue identified by its queueUrl.
-   * @param {string} queueUrl - URL of the queue to be purged
+   * @param {string} queueNameOrUrl - The name or URL of the queue to be purged.
    * @returns {Promise<void>} - Promise that resolves when the queue is purged
    */
-  async purgeQueue(queueUrl) {
+  async purgeQueue(queueNameOrUrl) {
+    const queueUrl = await this.#toQueueUrl(queueNameOrUrl);
     const purgeQueueCommand = new PurgeQueueCommand({ QueueUrl: queueUrl });
+
     try {
       await this.sqsClient.send(purgeQueueCommand);
       this.log.info(`Success, queue purged. QueueUrl: ${queueUrl}`);
@@ -58,6 +73,44 @@ class SQS {
       this.log.error(`Queue purge failed. Type: ${type}, Code: ${code}, Message: ${msg}`);
       throw e;
     }
+  }
+
+  /**
+   * Retrieve the URL for a queue if it's not a URL already.
+   *
+   * If a queue name is passed in rather than a URL, the URL for that queue is retrieved
+   * from AWS and returned.
+   * @param {string|URL} queueNameOrUrl - The name or URL of the queue.
+   * @returns {Promise<string>} - The URL of the queue.
+   */
+  async #toQueueUrl(queueNameOrUrl) {
+    if (typeof queueNameOrUrl !== 'string' || URL.canParse(queueNameOrUrl)) {
+      return String(queueNameOrUrl);
+    }
+
+    const namedQueueURLs = this.#namedQueueURLs;
+    let queueUrl = namedQueueURLs.get(queueNameOrUrl);
+    if (!queueUrl) {
+      queueUrl = this.#retrieveQueueUrl(queueNameOrUrl);
+      namedQueueURLs.set(queueNameOrUrl, queueUrl);
+    }
+
+    return queueUrl;
+  }
+
+  /**
+   * Retrieve the URL for a queue by its name.
+   *
+   * @param {string} queueName - The name of the queue.
+   * @returns {Promise<string>} - The URL of the queue.
+   */
+  async #retrieveQueueUrl(queueName) {
+    const response = await this.sqsClient.send(new GetQueueUrlCommand({ QueueName: queueName }));
+    const url = response.QueueUrl;
+    if (!url || !URL.canParse(url)) {
+      throw new Error(`Unknown queue name: ${queueName}`);
+    }
+    return url;
   }
 }
 
