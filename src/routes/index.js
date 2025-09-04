2f52f5080a9dca74de22c1568d9205367d1fe832
Dominique Jäggi <djaeggi@adobe.com>
2025-09-04 08:25:36 +0200

    fix: test

--- src/routes/index.js
+++ src/routes/index.js
@@ -102,7 +102,6 @@ export default function getRouteHandlers(
   consentBannerController,
   scrapeController,
   scrapeJobController,
-  mcpController,
   paidController,
   trafficController,
   fixesController,
