44f573ee6ea7a1042c7d6183ed5ea38ae6a7508a
Daniel Huser <djc@adobe.com>
2025-11-18 16:00:12 +0100

    added endpoints for UTM_Campaign

--- src/controllers/paid/traffic.js
+++ src/controllers/paid/traffic.js
@@ -293,6 +293,11 @@ function TrafficController(context, log, env) {
     getPaidTrafficByUrlChannel: async () => fetchPaidTrafficData(['path', 'trf_channel'], TrafficDataWithCWVDto),
     getPaidTrafficByUrlChannelDevice: async () => fetchPaidTrafficData(['path', 'trf_channel', 'device'], TrafficDataWithCWVDto),
     getPaidTrafficByUrlChannelPlatformDevice: async () => fetchPaidTrafficData(['path', 'trf_channel', 'trf_platform', 'device'], TrafficDataWithCWVDto),
+
+    // Campaign Performance endpoints
+    // getPaidTrafficByCampaign (see above)
+    getPaidTrafficByCampaignChannelDevice: async () => fetchPaidTrafficData(['utm_campaign', 'trf_channel', 'device'], TrafficDataWithCWVDto),
+    getPaidTrafficByCampaignChannelPlatformDevice: async () => fetchPaidTrafficData(['utm_campaign', 'trf_channel', 'trf_platform', 'device'], TrafficDataWithCWVDto),
   };
 }
 
--- test/routes/index.test.js
+++ test/routes/index.test.js
@@ -186,6 +186,8 @@ describe('getRouteHandlers', () => {
     getPaidTrafficByUrlChannel: sinon.stub(),
     getPaidTrafficByUrlChannelDevice: sinon.stub(),
     getPaidTrafficByUrlChannelPlatformDevice: sinon.stub(),
+    getPaidTrafficByCampaignChannelDevice: sinon.stub(),
+    getPaidTrafficByCampaignChannelPlatformDevice: sinon.stub(),
   };
 
   const mockFixesController = {
@@ -470,6 +472,8 @@ describe('getRouteHandlers', () => {
       'GET /sites/:siteId/traffic/paid/url-channel',
       'GET /sites/:siteId/traffic/paid/url-channel-device',
       'GET /sites/:siteId/traffic/paid/url-channel-platform-device',
+      'GET /sites/:siteId/traffic/paid/campaign-channel-device',
+      'GET /sites/:siteId/traffic/paid/campaign-channel-platform-device',
       'GET /tools/scrape/jobs/:jobId',
       'GET /tools/scrape/jobs/:jobId/results',
       'GET /tools/scrape/jobs/by-date-range/:startDate/:endDate/all-jobs',
@@ -611,6 +615,8 @@ describe('getRouteHandlers', () => {
     expect(dynamicRoutes['GET /sites/:siteId/traffic/paid/url-channel'].handler).to.equal(mockTrafficController.getPaidTrafficByUrlChannel);
     expect(dynamicRoutes['GET /sites/:siteId/traffic/paid/url-channel-device'].handler).to.equal(mockTrafficController.getPaidTrafficByUrlChannelDevice);
     expect(dynamicRoutes['GET /sites/:siteId/traffic/paid/url-channel-platform-device'].handler).to.equal(mockTrafficController.getPaidTrafficByUrlChannelPlatformDevice);
+    expect(dynamicRoutes['GET /sites/:siteId/traffic/paid/campaign-channel-device'].handler).to.equal(mockTrafficController.getPaidTrafficByCampaignChannelDevice);
+    expect(dynamicRoutes['GET /sites/:siteId/traffic/paid/campaign-channel-platform-device'].handler).to.equal(mockTrafficController.getPaidTrafficByCampaignChannelPlatformDevice);
     expect(dynamicRoutes['GET /sites/:siteId/files'].handler).to.equal(mockScrapeController.getFileByKey);
     expect(dynamicRoutes['GET /sites/:siteId/files'].paramNames).to.deep.equal(['siteId']);
     expect(dynamicRoutes['GET /tools/scrape/jobs/:jobId'].handler).to.equal(mockScrapeJobController.getScrapeJobStatus);
