1a58875f4e7b2197813bc32ae05d1fb2f2fec47e
Dominique Jäggi <1872195+solaris007@users.noreply.github.com>
2025-12-01 10:27:10 +0100

    fix: config update issues after update of helix-deploy (#1589)
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: semantic-release-bot <semantic-release-bot@martynus.net>

--- test/index.test.js
+++ test/index.test.js
@@ -18,7 +18,7 @@ import sinonChai from 'sinon-chai';
 import sinon from 'sinon';
 import AccessControlUtil from '../src/support/access-control-util.js';
 
-import { main } from '../src/index.js';
+import { main } from './utils.js';
 
 use(sinonChai);
 
@@ -98,6 +98,13 @@ describe('Index Tests', () => {
         Audit: {
           findBySiteIdAndAuditTypeAndAuditedAt: sinon.stub().resolves(mockAuditData),
         },
+        Configuration: {
+          findLatest: sinon.stub().resolves({
+            getVersion: () => 1,
+            getHandlers: () => ({}),
+            isHandlerEnabledForSite: sinon.stub().returns(false),
+          }),
+        },
         Organization: {
           findById: sinon.stub().resolves({
             getId: () => 'default',
@@ -117,6 +124,9 @@ describe('Index Tests', () => {
           findById: sinon.stub().resolves({
             id: 'site-id',
           }),
+        },
+        ApiKey: {
+          findByHashedApiKey: sinon.stub().resolves(null),
         },
         Opportunity: {},
         Suggestion: {},
