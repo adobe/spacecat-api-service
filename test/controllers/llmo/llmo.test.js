f38c7443fb1141d5a124de6624256e4f8ee6d6e9
ani hammond <annhammo@MacBookPro.lan>
2026-05-06 19:02:22 -0500

    test

--- test/controllers/llmo/llmo.test.js
+++ test/controllers/llmo/llmo.test.js
@@ -185,7 +185,8 @@ describe('LlmoController', () => {
     },
   });
 
-  before(async () => {
+  before(async function llmoControllerSetup() {
+    this.timeout(10000);
     triggerBrandProfileAgentStub = sinon.stub().resolves('exec-123');
     updateModifiedByDetailsStub = sinon.stub();
 
